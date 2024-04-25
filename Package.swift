99c13976434730ad1c7bb9ffaf0d46bf2f7de914
Handy <handy.kwok@gmail.com>
2024-04-25 15:40:07 +0700

    add colors and icons

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "AHResources",
+    platforms: [
+            .iOS(.v16) // Set the minimum iOS version to iOS 16
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
--- Sources/AHResources/AHResources.swift
+++ Sources/AHResources/AHResources.swift
@@ -1,2 +1,24 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public struct AHResources {
+    public init() {}
+}
+
+extension AHResources {
+    public struct Colors {
+        public init() {}
+        
+        public static let cardBackground = Color("cardBackground")
+    }
+}
+
+extension AHResources {
+    public struct Icons {
+        public init() {}
+        
+        public static let sunMax = Image(systemName: "sun.max")
+    }
+}
